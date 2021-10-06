0f6528cb76466fec7719a54661b54b67f5133248
zhangming <zhangming@sensetime.com>
2021-10-06 16:28:57 +0800

    add doc

--- gobigger/agents/__init__.py
+++
@@ -1,2 +0,0 @@
-from .base_agent import BaseAgent
-from .bot_agent import BotAgent
--- gobigger/render/__init__.py
+++
@@ -1,3 +0,0 @@
-from .base_render import BaseRender
-from .realtime_render import RealtimeRender, RealtimePartialRender
-from .env_render import EnvRender
