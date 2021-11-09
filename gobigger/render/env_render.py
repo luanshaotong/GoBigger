bacaf3e22c1b73d5b4d6974292b104433de1b785
zhangming <zhangming@sensetime.com>
2021-11-09 18:43:12 +0800

    add profile

--- gobigger/render/env_render.py
+++ gobigger/render/env_render.py
@@ -330,19 +330,19 @@ class EnvRender(BaseRender):
         # assert len(rgb.shape) == 2
         # h, w = rgb.shape
         # for i in range(player_num):
-        #     features.append((rgb==PLAYER_COLORS_GRAYSCALE[i][0]).astype(int))
-        # features.append((rgb==FOOD_COLOR_GRAYSCALE[0]).astype(int))
-        # features.append((rgb==SPORE_COLOR_GRAYSCALE[0]).astype(int))
-        # features.append((rgb==THORNS_COLOR_GRAYSCALE[0]).astype(int))
+        #     features.append((rgb==PLAYER_COLORS_GRAYSCALE[i]).astype(int))
+        # features.append((rgb==FOOD_COLOR_GRAYSCALE).astype(int))
+        # features.append((rgb==SPORE_COLOR_GRAYSCALE).astype(int))
+        # features.append((rgb==THORNS_COLOR_GRAYSCALE).astype(int))
 
         total_len = player_num + 3
         if not hasattr(self, 'to_equal_arr'):
             arr = []
             for i in range(player_num):
-                arr.append(PLAYER_COLORS_GRAYSCALE[i][0])
-            arr.append(FOOD_COLOR_GRAYSCALE[0])
-            arr.append(SPORE_COLOR_GRAYSCALE[0])
-            arr.append(THORNS_COLOR_GRAYSCALE[0])
+                arr.append(PLAYER_COLORS_GRAYSCALE[i])
+            arr.append(FOOD_COLOR_GRAYSCALE)
+            arr.append(SPORE_COLOR_GRAYSCALE)
+            arr.append(THORNS_COLOR_GRAYSCALE)
             self.to_equal_arr = np.array(arr, dtype=np.int)
             self.to_equal_arr = np.reshape(total_len, 1, 1)
         rgb = rgb.reshape(1, h, w)
